3970760296dbcdb3305cbc901a0291296a785402
Shaira <sil21019@byui.edu>
2025-02-08 21:04:30 +0000

    first commit

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Axis;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
