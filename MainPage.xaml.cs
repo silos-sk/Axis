945787674550ac7607011aadf57198b9ab9449e6
Shaira <sil21019@byui.edu>
2025-02-08 22:57:21 +0000

    home - day curve btns added

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,16 +9,27 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	// private void OnCounterClicked(object sender, EventArgs e)
+	// {
+	// 	count++;
+
+	// 	if (count == 1)
+	// 		CounterBtn.Text = $"Clicked {count} time";
+	// 	else
+	// 		CounterBtn.Text = $"Clicked {count} times";
+
+	// 	SemanticScreenReader.Announce(CounterBtn.Text);
+	// }
+	private void OnProtocolClicked(object sender, EventArgs e)
 	{
 		count++;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+			ThreePt.Text = $"Clicked {count} time";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+			ThreePt.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		SemanticScreenReader.Announce(ThreePt.Text);
 	}
 }
 
