9a4fbcad7734b93bcf9fa4b9e8fc6594f907a715
Shaira <sil21019@byui.edu>
2025-02-09 13:56:58 +0000

    sample list view added

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,6 +11,15 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+		List<SteroidRegime> Regimes=
+        [
+            new SteroidRegime { Time="8:00", Dose="20" },
+			new SteroidRegime { Time="12:00", Dose="10" },
+			new SteroidRegime { Time="17:00", Dose="10" },
+		];
+
+		listRegimes.ItemsSource = Regimes;
 	}
 
 	private void OnProtocolClicked(object sender, EventArgs e)
@@ -19,7 +28,18 @@ public partial class MainPage : ContentPage
 		Shell.Current.GoToAsync(nameof(Protocols));
 	}
 
-	
+	public class Patient
+    {
+        public string? Name { get; set; }
+        public int? MRN { get; set; }
+        public string? Medication { get; set; }
+    }
+
+    public class SteroidRegime
+    {
+        public string? Time { get; set; }
+        public string? Dose { get; set; }
+    }
 
 }
 
