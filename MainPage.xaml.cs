2580fe99f5c250ae3cd4dc93b82214fad99c355a
Shaira <sil21019@byui.edu>
2025-02-09 13:14:14 +0000

    clean up

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Authentication;
 using ObjCRuntime;
 using Axis.Views;
+using Axis;
 
 namespace Axis;
 
@@ -12,30 +13,13 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	// private void OnCounterClicked(object sender, EventArgs e)
-	// {
-	// 	count++;
-
-	// 	if (count == 1)
-	// 		CounterBtn.Text = $"Clicked {count} time";
-	// 	else
-	// 		CounterBtn.Text = $"Clicked {count} times";
-
-	// 	SemanticScreenReader.Announce(CounterBtn.Text);
-	// }
 	private void OnProtocolClicked(object sender, EventArgs e)
 	{
-		// count++;
-
-		// if (count == 1)
-		// 	ThreePt.Text = $"Clicked {count} time";
-		// else
-		// 	ThreePt.Text = $"Clicked {count} times";
-
-		// SemanticScreenReader.Announce(ThreePt.Text);
 
 		Shell.Current.GoToAsync(nameof(Protocols));
 	}
+
+	
 
 }
 
